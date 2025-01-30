2c264675e7ed58f96efd39b2a22ae5d5cb3e0a7c
Julieta Quintana <julieta.quintana@bootcamp-upgrade.com>
2025-01-30 09:28:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,30 +33,29 @@ st.markdown(
     unsafe_allow_html=True
 )
 st.sidebar.image("Logo_blue.png", use_container_width=True)
-selected_tab = st.sidebar.radio("Selecciona una pestaña:", ["New Campaign Evaluation", "Testing Campaigns"])
+selected_tab = st.sidebar.radio("",["New Campaign Evaluation", "Testing Campaigns"])
 if selected_tab == "New Campaign Evaluation":
     st.title("New Campaign Evaluation")
     sales = st.number_input("Sales per day (provided by client):", min_value=0, value=0, step=1)
     revenue = st.number_input("Average revenue per sale (provided by client):", min_value=0.0, value=0.0, step=0.01)
-    conversion_share = st.selectbox("Select Country:",['Scandinavia','Rest of Europe','USA and WW'])
-    tipo_conversion_share = {'Scandinavia': 5.4,'Rest of Europe': 4.12, 'WW': 3.67}
+    moneda = st.selectbox("Select Currency:", ["USD", "GBP", "RUB", "DKK", "NOK", "SEK", "EUR"])
+    conversion_share = st.selectbox("Select Country:", ['Scandinavia', 'Rest of Europe', 'USA and WW'])
+    tipo_conversion_share = {'Scandinavia': 5.4, 'Rest of Europe': 4.12, 'USA and WW': 3.67}
     commission = st.number_input("Commission (%):", min_value=0.0, max_value=100.0, value=0.0, step=0.1)
+    
+    tipo_cambio = {
+        ("USD", "EUR"): 0.9602, ("GBP", "EUR"): 1.1936,
+        ("RUB", "EUR"): 0.0098, ("DKK", "EUR"): 0.130,
+        ("NOK", "EUR"): 0.085, ("SEK", "EUR"): 0.087, ("EUR", "EUR"): 1.0
+    }
+        
     if st.button("Calculate"):
         conversion_share_decimal = tipo_conversion_share.get(conversion_share) / 100
         commission_decimal = commission / 100
         potential_revenue = (sales * conversion_share_decimal) * revenue * commission_decimal * 30
-        st.success(f"Potential revenue: {potential_revenue:,.2f}")
-    st.header("Currency Converter")
-    monto = st.number_input("Amount in source currency:", min_value=0.0, value=0.0, step=0.01)
-    moneda_origen = st.selectbox("Select source currency:", ["USD", "GBP", "RUB", "DKK" , "NOK","SEK"])
-    moneda_destino = st.selectbox("Select target currency:", ["EUR"])
-    tipo_cambio = {
-        ("USD", "EUR"): 0.9602, ("GBP", "EUR"): 1.1936,
-        ("RUB ", "USD"): 0.0098, ("DKK", "EUR") : 0.130,
-        ("NOK" , "EUR"): 0.085, ("SEK", "EUR"): 0.087
-    }
-    resultado_conversion = monto * tipo_cambio.get((moneda_origen, moneda_destino), 1.0) if moneda_origen != moneda_destino else monto
-    st.write(f"Converted amount: {resultado_conversion:,.2f} {moneda_destino}")
+        potential_revenue_converted = potential_revenue * tipo_cambio.get((moneda, "EUR"), 1.0)
+        st.success(f"Potential revenue: {potential_revenue_converted:,.2f} EUR")
+
 elif selected_tab == "Testing Campaigns":
     st.title("Testing Campaigns")
     media_cost = st.number_input("Accumulated media cost (EUR):", min_value=0.0, max_value=100000.0, value=0.0, step=0.1)
@@ -64,7 +63,9 @@ elif selected_tab == "Testing Campaigns":
     if st.button("Calculate"):
         if days > 0:
             potential_revenue_test = (media_cost / days) * 30
-            st.success(f"Potential media coast: {potential_revenue_test:,.2f}")
+            st.success(f"Potential media coast: {potential_revenue_test:,.2f} EUR")
         else:
             st.error("The number of days must be greater than 0 to calculate the average cost.")
+
+
             
